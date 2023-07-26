329a7154466b7744c6223f04e860c55ef9db7ef6
MatheusPMatheus <mpeima@yahoo.com.br>
2023-07-25 22:20:40 -0300

    parte 2 aula 25.07

--- tabuada.js
+++ tabuada.js
@@ -1,15 +1,29 @@
 const frm =  document.querySelector("form");
 const resp = document.querySelector("pre");
+const respPar = document.querySelector("#outPar");
+const respImpar = document.querySelector("#outImpar");
+
 
 frm.addEventListener("submit", (e)=> {
     e.preventDefault();
     const numero = Number(frm.inNumero.value);
 
     let resposta = "";
+    let respostaPar = 0;
+    let respostaImpar = 0;
     for(let i = 1; i<=10; i ++){
+        if(numero*i%2 === 0){
+            respostaPar += numero*i;
+        }else{
+            respostaImpar += numero*i;
+        }
         resposta += numero + " X " + i + " = "+ (numero*i)+"\n";
     }
-
+    
+    
     resp.innerText = resposta;
+    respPar.innerText = "A soma dos pares é: " + respostaPar+".";
+    respImpar.innerText ="A soma dos impares é: " + respostaImpar+".";
+
     
 })
